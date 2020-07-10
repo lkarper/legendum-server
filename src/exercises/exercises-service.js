4ef71c55ec71af0d272aa8b5f0ff7d26ec5fd13f
Luther Karper <luther.karper@live.com>
2020-07-10 14:31:03 -0400

    Fix typo

--- src/exercises/exercises-service.js
+++ src/exercises/exercises-service.js
@@ -1,7 +1,7 @@
 const ExercisesService = {
     getExercisesLearnById(db, id) {
         return db
-            .select('lel.id', 'lel.exercise_id', 'lel.page', 'lel.text', 'lel.image_url', 'le.exercise_title', 'le.exercise_translation', 'lelh.hints')
+            .select('lel.id', 'lel.exercise_id', 'lel.page', 'lel.text', 'lel.image_url','lel.image_alt_text', 'le.exercise_title', 'le.exercise_translation', 'lelh.hints')
             .from('legendum_exercises_learn AS lel')
             .where('lel.exercise_id', id)
             .join('legendum_exercises AS le', 'lel.exercise_id', 'le.id')
