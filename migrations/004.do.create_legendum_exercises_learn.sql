cd2d9a0166bdb787894fa3afaeddd36cbd69a9c5
Luther Karper <luther.karper@live.com>
2020-07-10 17:24:00 -0400

    Fix typo

--- migrations/004.do.create_legendum_exercises_learn.sql
+++ migrations/004.do.create_legendum_exercises_learn.sql
@@ -2,7 +2,7 @@ CREATE TABLE legendum_exercises_learn (
     id SERIAL PRIMARY KEY,
     exercise_id INTEGER
         REFERENCES legendum_exercises(id) ON DELETE CASCADE NOT NULL,
-    page INTEGER NOT NULL UNIQUE,
+    page INTEGER NOT NULL,
     text TEXT NOT NULL,
     image_url TEXT,
     image_alt_text TEXT
