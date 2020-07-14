c367969f1e2bf891b80c3f2764eefe4ec76cda8f
Luther Karper <luther.karper@live.com>
2020-07-14 14:25:00 -0400

    Implement jwt authorization

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/legendum',
+    JWT_SECRET: process.env.JWT_SECRET || '123NotAParticularyGreatSecret!!!',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
   }
