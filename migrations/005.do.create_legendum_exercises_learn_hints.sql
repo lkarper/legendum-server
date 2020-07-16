c976d4052abb2c4d4fb45876ae4d4f142f352848
Luther Karper <luther.karper@live.com>
2020-07-15 20:44:56 -0400

    Update migraion and seed files

--- migrations/005.do.create_legendum_exercises_learn_hints.sql
+++ migrations/005.do.create_legendum_exercises_learn_hints.sql
@@ -2,5 +2,6 @@ CREATE TABLE legendum_exercises_learn_hints (
     id SERIAL PRIMARY KEY,
     exercise_page_id INTEGER
         REFERENCES legendum_exercises_learn(id) ON DELETE CASCADE NOT NULL,
-    hints TEXT NOT NULL
+    hint_order_number INTEGER NOT NULL,
+    hint TEXT NOT NULL
 );
---
+++ migrations/008.undo.create_legendum_saved_notes.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS legendum_saved_notes;
