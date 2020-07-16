bfd62a2a04c959c197f4239a134d59fc24b7f092
Luther Karper <luther.karper@live.com>
2020-07-15 20:46:39 -0400

    Update ExercisesService
    Updated getExercisesLearnById so that hints are returned as an array
    nested beneath the exercise to which they pertain.

--- src/exercises/exercises-service.js
+++ src/exercises/exercises-service.js
@@ -1,7 +1,27 @@
 const ExercisesService = {
     getExercisesLearnById(db, id) {
         return db
-            .select('lel.id', 'lel.exercise_id', 'lel.page', 'lel.text', 'lel.image_url','lel.image_alt_text', 'le.exercise_title', 'le.exercise_translation', 'lelh.hints')
+            .select(
+                'lel.id', 
+                'lel.exercise_id', 
+                'lel.page', 
+                'lel.text', 
+                'lel.image_url',
+                'lel.image_alt_text', 
+                'le.exercise_title', 
+                'le.exercise_translation',
+                db.raw(
+                    `(
+                        select array_to_json(
+                            array_agg(row_to_json(h))
+                        ) from (
+                            select id, hint_order_number, hint 
+                            from legendum_exercises_learn_hints AS lelh
+                            where lelh.exercise_page_id=lel.id
+                        ) h
+                    ) as hints`
+                )
+            )
             .from('legendum_exercises_learn AS lel')
             .where('lel.exercise_id', id)
             .join('legendum_exercises AS le', 'lel.exercise_id', 'le.id')
