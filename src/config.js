6072c547db729764a869cd729e612bc6e02bbeaa
Luther Karper <luther.karper@live.com>
2020-08-12 14:49:00 -0400

    Fix typo

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/legendum',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/legendum',
     JWT_SECRET: process.env.JWT_SECRET || '123NotAParticularyGreatSecret!!!',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
   }
