8463c3ed52bc2290273445750c69e54a8d9202b0
Luther Karper <luther.karper@live.com>
2020-10-05 20:20:49 -0400

    Update tests for exercisesRouter
    Added tests for PATCH requests for route '/api/exercises/:exercise_id'.

--- test/exercises.spec.js
+++ test/exercises.spec.js
@@ -69,7 +69,7 @@ describe.only('Exercises endpoints', () => {
         });
     });
 
-    describe.only('POST /api/exercises', () => {
+    describe('POST /api/exercises', () => {
         const newExercise = exercises[0];
 
         context('Given that there is no auth header', () => {
@@ -100,10 +100,9 @@ describe.only('Exercises endpoints', () => {
         });
 
         context('Given that the user does have admin privileges', () => {
-            beforeEach('seed users', () => helpers.seedUsers(db, testUsers));
-            beforeEach('seed stories', () => helpers.seedStories(db, stories));
-
             context('Given that the chapter number does not exist', () => {
+                beforeEach('seed users', () => helpers.seedUsers(db, testUsers));
+                beforeEach('seed stories', () => helpers.seedStories(db, stories));
                 it('responds with 404 and an error message', () => {
                     const postAttempt = { ...newExercise, chapter_number: 1000 };
                     return supertest(app)
@@ -117,51 +116,246 @@ describe.only('Exercises endpoints', () => {
             });
 
             context('Given that the chapter number does exist', () => {
-                const requiredFields = [
-                    'chapter_number',
-                    'exercise_title', 
-                    'exercise_translation',
-                ];
+                context('Given an incorrectly formed request body', () => {
+                    beforeEach('seed users', () => helpers.seedUsers(db, testUsers));
+                    beforeEach('seed stories', () => helpers.seedStories(db, stories));
+                    const requiredFields = [
+                        'chapter_number',
+                        'exercise_title', 
+                        'exercise_translation',
+                    ];
+    
+                    requiredFields.forEach(field => {
+                        const postAttempt = { ...newExercise };
+                        delete postAttempt[field];
+    
+                        it(`responds with 400 and an error message when '${field}' is missing`, () => {
+                            return supertest(app)
+                                .post('/api/exercises')
+                                .set('Authorization', helpers.makeAuthHeader(adminUser))
+                                .send(postAttempt)
+                                .expect(400, {
+                                    error: `Missing '${field}' in request body`,
+                                });
+                        });
+                    });
+                });
+            
+                context('Given that the request field is complete', () => {
+                    context('Given that the chapter_number is already in use', () => {
+                        before('seed exercises', () => helpers.seedExercises(db, testUsers, stories, exercises));
+                        const postAttempt = { ...newExercise };
+                        it('responds with 400 and an error message', () => {
+                            return supertest(app)
+                                .post('/api/exercises')
+                                .set('Authorization', helpers.makeAuthHeader(adminUser))
+                                .send(postAttempt)
+                                .expect(400, {
+                                    error: `Chapter number is already in use`,
+                                });
+                        });
+                    });
 
-                requiredFields.forEach(field => {
-                    const postAttempt = { ...newExercise };
-                    delete postAttempt[field];
+                    context('Given that the chapter_number is not in use', () => {
+                        beforeEach('seed users', () => helpers.seedUsers(db, testUsers));
+                        beforeEach('seed stories', () => helpers.seedStories(db, stories));
+                        it('responds with 201 and creates a new exercise', () => {
+                            return supertest(app)
+                                .post('/api/exercises')
+                                .set('Authorization', helpers.makeAuthHeader(adminUser))
+                                .send(newExercise)
+                                .expect(201)
+                                .expect(res => {
+                                    expect(res.headers.location).to.eql(`/api/exercises/${res.body.id}`);
+                                    expect(res.body).to.have.property('id');
+                                    expect(res.body.chapter_number).to.eql(newExercise.chapter_number);
+                                    expect(res.body.exercise_title).to.eql(newExercise.exercise_title);
+                                    expect(res.body.exercise_translation).to.eql(newExercise.exercise_translation);
+                                })
+                                .then(postRes =>
+                                    supertest(app)
+                                        .get(`/api/exercises/${postRes.body.id}`)
+                                        .expect(res => {
+                                            expect(res.body.id).to.eql(postRes.body.id);
+                                            expect(res.body.chapter_number).to.eql(newExercise.chapter_number);
+                                            expect(res.body.exercise_title).to.eql(newExercise.exercise_title);
+                                            expect(res.body.exercise_translation).to.eql(newExercise.exercise_translation);
+                                        })
+                                );
+                        });
+                    });
+                });
+            });
+        });
+    });
 
-                    it(`responds with 400 and an error message when '${field}' is missing`, () => {
+    describe('GET /api/exercises/:exercise_id', () => {
+        context('Given that the exercise with exercise_id does not exist', () => {
+            it('responds with 404 and an error message', () => {
+                return supertest(app)
+                .get('/api/exercises/1')
+                .expect(404, {
+                    error: {
+                        message: `Exercise doesn't exist`,
+                    },
+                });
+            });
+        });
+
+        context('Given that the exercise with exercise_id does exist', () => {
+            before('seed exercises', () => helpers.seedExercises(db, testUsers, stories, exercises));
+            it('responds with 200 and the exercise', () => {
+                const testExercise = exercises[0];
+                return supertest(app)
+                    .get(`/api/exercises/${testExercise.id}`)
+                    .expect(200, testExercise);
+            });
+        });
+    });
+
+    describe('DELETE /api/exercises/:exercise_id', () => {
+        context('Given that the exercise with exercise_id does not exist', () => {
+            it('responds with 404 and an error message', () => {
+                return supertest(app)
+                .delete('/api/exercises/1')
+                .expect(404, {
+                    error: {
+                        message: `Exercise doesn't exist`,
+                    },
+                });
+            });
+        });
+
+        context('Given that the exercise with exercise_id does exist', () => {
+            beforeEach('seed exercises', () => helpers.seedExercises(db, testUsers, stories, exercises));
+
+            context('Given that there is no auth header', () => {
+                it('responds with 401 and an error message', () => {
+                    return supertest(app)
+                        .delete('/api/exercises/1')
+                        .expect(401, {
+                            error: 'Missing bearer token',
+                        });
+                });
+            });
+
+            context('Given that there is an auth header', () => {
+                context('Given that the user does not have admin privileges', () => {
+                    it('responds with 401 and an error message', () => {
                         return supertest(app)
-                            .post('/api/exercises')
-                            .set('Authorization', helpers.makeAuthHeader(adminUser))
-                            .send(postAttempt)
-                            .expect(400, {
-                                error: `Missing '${field}' in request body`,
+                            .delete('/api/exercises/1')
+                            .set('Authorization', helpers.makeAuthHeader(nonAdminUser))
+                            .expect(401, {
+                                error: 'This account does not have admin privileges',
                             });
                     });
                 });
 
-                context('Given that the request field is complete', () => {
-                    it('responds with 201 and creates a new exercise', () => {
+                context('Given that the user does have admin privileges', () => {
+                    it('responds with 204 and removes the exercise', () => {
+                        const exerciseToRemove = exercises[0];
+                        const expectedExercises = exercises.filter(e => e.id !== exerciseToRemove.id);
                         return supertest(app)
-                            .post('/api/exercises')
+                            .delete(`/api/exercises/${exerciseToRemove.id}`)
                             .set('Authorization', helpers.makeAuthHeader(adminUser))
-                            .send(newExercise)
-                            .expect(201)
-                            .expect(res => {
-                                expect(res.headers.location).to.eql(`/api/exercises/${res.body.id}`);
-                                expect(res.body).to.have.property('id');
-                                expect(res.body.chapter_number).to.eql(newExercise.chapter_number);
-                                expect(res.body.exercise_title).to.eql(newExercise.exercise_title);
-                                expect(res.body.exercise_translation).to.eql(newExercise.exercise_translation);
-                            })
-                            .then(postRes =>
+                            .expect(204)
+                            .then(() => 
                                 supertest(app)
-                                    .get(`/api/exercises/${postRes.body.id}`)
-                                    .expect(res => {
-                                        expect(res.body.id).to.eql(postRes.body.id);
-                                        expect(res.body.chapter_number).to.eql(newExercise.chapter_number);
-                                        expect(res.body.exercise_title).to.eql(newExercise.exercise_title);
-                                        expect(res.body.exercise_translation).to.eql(newExercise.exercise_translation);
-                                    })
+                                    .get('/api/exercises')
+                                    .expect(200, expectedExercises)
                             );
+                    });
+                });
+            });
+        });        
+    });
+
+    describe('PATCH /api/exercises/:exercise_id', () => {
+        context('Given that the exercise with exercise_id does not exist', () => {
+            it('responds with 404 and an error message', () => {
+                return supertest(app)
+                .patch('/api/exercises/1')
+                .send({ exercise_title: 'Update' })
+                .expect(404, {
+                    error: {
+                        message: `Exercise doesn't exist`,
+                    },
+                });
+            });
+        });
+
+        context('Given that the exercise with exercise_id does exist', () => {
+            beforeEach('seed exercises', () => helpers.seedExercises(db, testUsers, stories, exercises));
+            
+            context('Given that there is no auth header', () => {
+                it('responds with 401 and an error message', () => {
+                    return supertest(app)
+                        .patch('/api/exercises/1')
+                        .send({ exercise_title: 'Update' })
+                        .expect(401, {
+                            error: 'Missing bearer token',
+                        });
+                });
+            });
+
+            context('Given that there is an auth header', () => {
+                context('Given that the user does not have admin privileges', () => {
+                    it('responds with 401 and an error message', () => {
+                        return supertest(app)
+                            .patch('/api/exercises/1')
+                            .set('Authorization', helpers.makeAuthHeader(nonAdminUser))
+                            .send({ exercise_title: 'Update' })
+                            .expect(401, {
+                                error: 'This account does not have admin privileges',
+                            });
+                    });
+                });
+
+                context('Given that the user does have admin privileges', () => {
+                    context('Given that the chapter number does not exist', () => {
+                        it('responds with 404 and an error message', () => {
+                            return supertest(app)
+                                .patch('/api/exercises/1')
+                                .set('Authorization', helpers.makeAuthHeader(adminUser))
+                                .send({ chapter_number: 1000 })
+                                .expect(404, {
+                                    error: `Chapter doesn't exist`,
+                                });
+                        });
+                    });
+
+                    context('Given that the chapter number does exist', () => {
+                        context('Given that the chapter_number is already in use', () => {
+                            it('responds with 400 and an error message', () => {
+                                return supertest(app)
+                                    .patch('/api/exercises/1')
+                                    .set('Authorization', helpers.makeAuthHeader(adminUser))
+                                    .send({ chapter_number: 2 })
+                                    .expect(400, {
+                                        error: `Chapter number is already in use`,
+                                    });
+                            });
+                        });
+
+                        it('responds with 204 and updates the exercise', () => {
+                            const updatedExercise = {
+                                id: 1,
+                                chapter_number: 1,
+                                exercise_title: 'Updated',
+                                exercise_translation: 'Updated',
+                            };
+
+                            return supertest(app)
+                                .patch(`/api/exercises/${updatedExercise.id}`)
+                                .set('Authorization', helpers.makeAuthHeader(adminUser))
+                                .send(updatedExercise)
+                                .expect(204)
+                                .then(() => 
+                                    supertest(app)
+                                        .get(`/api/exercises/${updatedExercise.id}`)
+                                        .expect(200, updatedExercise)
+                                );
+                        });
                     });
                 });
             });
